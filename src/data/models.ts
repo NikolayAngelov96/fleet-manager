35913e05225ec07eb75ab7ead392edcd04a87e36
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-11-26 09:34:53 +0200

    add data class models

---
+++ src/data/models.ts
@@ -0,0 +1,44 @@
+export abstract class Vehicle {
+  constructor(
+    public id: string,
+    public make: string,
+    public model: string,
+    public rentalPrice: number,
+    public rentedTo: string | null
+  ) {}
+}
+
+type CarBodyType = "sedan" | "suv" | "hatchback";
+
+type Transmissions = "manual" | "automatic";
+
+export class Car extends Vehicle {
+  constructor(
+    id: string,
+    make: string,
+    model: string,
+    rentalPrice: number,
+    rentedTo: string | null,
+    public bodyType: CarBodyType,
+    public numberOfSeats: number,
+    public transmission: Transmissions
+  ) {
+    super(id, make, model, rentalPrice, rentedTo);
+  }
+}
+
+type TruckCargoType = "box" | "flatbed" | "van";
+
+export class Truck extends Vehicle {
+  constructor(
+    id: string,
+    make: string,
+    model: string,
+    rentalPrice: number,
+    rentedTo: string | null,
+    public cargoType: TruckCargoType,
+    public capacity: number
+  ) {
+    super(id, make, model, rentalPrice, rentedTo);
+  }
+}
