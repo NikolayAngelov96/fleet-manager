3f6ecb53ecbc0acb9119695692f4fbae648f2d18
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-11-29 18:04:10 +0200

    add class for toast messages

---
+++ src/dom/Toaster.ts
@@ -0,0 +1,39 @@
+import { div } from "./dom";
+
+class Toaster {
+  private body: HTMLElement;
+  element: HTMLDivElement;
+
+  constructor();
+  constructor(parent?: HTMLElement) {
+    if (parent) {
+      this.body = parent;
+    } else {
+      this.body = document.body;
+    }
+    this.element = div({ className: "toaster" }) as HTMLDivElement;
+    this.element.style.top = "100px";
+  }
+
+  success(message: string) {
+    this.element.textContent = `✅ ${message}`;
+    this.displayElement();
+  }
+
+  error(message: string) {
+    this.element.textContent = `❌ ${message}`;
+    this.displayElement();
+  }
+
+  private displayElement() {
+    this.body.appendChild(this.element);
+
+    setTimeout(() => {
+      this.element.remove();
+    }, 3000);
+  }
+}
+
+const toast = new Toaster();
+
+export default toast;
